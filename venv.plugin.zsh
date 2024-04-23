c324b8e5cf5876894cbb0ebd98b07e9c1cad290b
Lucas Morais <lucascdemorais@gmail.com>
2024-04-23 12:31:49 -0300

    fixing load and adding unload venv resource

--- venv.plugin.zsh
+++ venv.plugin.zsh
@@ -3,11 +3,26 @@ autoload -U add-zsh-hook
 # Run 'source venv/bin/activate' automatically every time there's
 # a path "venv/bin/activate" file in the directory.
 load-venv() {
-  local venv_path="$(pwd)/venv/bin/activate"
-
+  local v="venv/bin/activate"
+  local venv_path="$(pwd)/$v"
+    
   if [ -f "$venv_path" ]; then
-    source $venv_path
+    print "Sourcing: \"$v\""
+    source "$venv_path"
   fi
 }
-add-zsh-hook chpwd load-nvmrc
+
+# Function to deactivate the virtual environment
+unload-venv() {
+  if [[ -n "$VIRTUAL_ENV" ]]; then
+    deactivate
+    source "$HOME/.zshrc"
+  fi
+}
+
+# Hook functions to directory change events
+add-zsh-hook chpwd unload-venv
+add-zsh-hook chpwd load-venv
+
+unload-venv
 load-venv
