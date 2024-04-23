ba200fbf7f73ccbd47fd12c080f4b3674f5a3f20
Lucas Morais <lucascdemorais@gmail.com>
2024-04-23 11:53:52 -0300

    init

---
+++ venv.plugin.zsh
@@ -0,0 +1,13 @@
+#!/bin/sh
+autoload -U add-zsh-hook
+# Run 'source venv/bin/activate' automatically every time there's
+# a path "venv/bin/activate" file in the directory.
+load-venv() {
+  local venv_path="$(pwd)/venv/bin/activate"
+
+  if [ -f "$venv_path" ]; then
+    source venv_path
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-venv
